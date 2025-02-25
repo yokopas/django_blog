210eaacd849b586c8cef07579fd1deb2854626c0
nik <nikolejevtovica@gmail.com>
2025-02-25 12:20:31 +0100

    create, edit, delete tests added

--- blog/tests.py
+++ blog/tests.py
@@ -43,3 +43,25 @@ class BlogTest(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Nice body counter")
         self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_createview(self):
+        response = self.client.post(
+            reverse("post_new"),
+            {"title": "New Title", "body": "New Text", "author": self.user.id},
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "New Title")
+        self.assertEqual(Post.objects.last().body, "New Text")
+
+    def test_updateview(self):
+        response = self.client.post(
+            reverse("post_edit", args="1"),
+            {"title": "Updated Title", "body": "Updated Body"},
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Updated Title")
+        self.assertEqual(Post.objects.last().body, "Updated Body")
+
+    def test_deleteview(self):
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
