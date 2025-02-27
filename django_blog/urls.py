6edb524eb4d373a3b5a02cb70ec4dbfee4868262
nik <nikolejevtovica@gmail.com>
2025-02-27 09:45:19 +0100

    log in

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -18,4 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("blog.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", include("blog.urls")),
+]
